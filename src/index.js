c8b43f9a0355cc6275df68ba032062bf55b1d5a9
lgarcia1212 <lanagav5@gmail.com>
2024-02-06 00:19:15 -0300

    actualizacion 2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-/* import http from 'http'
+import express from 'express';
+import { ProductManager } from './config/ProductManager.js';
 
-const app = http.createServer((req, res) => {
-    res.end
-})
-
-app.listen(8000, () => {
-    console.log("Server on port 8000")
-}) */
-
-import express from 'express'
-
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = 8000;
+const productManager = new ProductManager('./products.json');
 
 app.get('/', (req, res) => {
-    res.send("Servidor Express")
-})
+    res.send('Servidor Express');
+});
 
-app.get('/despedida', (req, res) => {
-    res.send("Gracias por utilizar el servidor")
-})
+app.get('/products', async (req, res) => {
+    const { limit } = req.query;
+    const prods = await productManager.getProducts();
+    const limite = parseInt(limit);
+
+    if (limite && limite > 0) {
+        const prodsLimit = prods.slice(0, limite);
+        res.send(prodsLimit);
+    } else {
+        res.send('Ingrese un número mayor a 0');
+    }
+});
+
+app.get('/productos/:pid', async (req, res) => {
+    const idProducto = req.params.pid;
+    const prod = await productManager.getProductById(idProducto);
+    res.send(prod);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
+    console.log(`Server on port ${PORT}`);
+});
