90eba1eea60ac19d970e7c5ddc3d2af534037a4c
lgarcia1212 <lanagav5@gmail.com>
2024-03-20 15:11:57 -0300

    actualizacion

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,21 +1,30 @@
- import { Schema, model } from 'mongoose'
+import { Schema, model } from 'mongoose'
 
- const cartSchema = new Schema({
-    products: [
-        {
-            id_prod: {
-                type: Schema.Types.ObjectId,
-                required: true,
-                ref: 'products'
-            },
-            quantity: {
-                type: Number,
-                required: true
+const cartSchema = new Schema({
+    products: {
+        type: [
+            {
+                id_prod: {
+                    type: Schema.Types.ObjectId,
+                    required: true,
+                    ref: 'products'
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                }
             }
-        }
-    ]
- })
+        ],
+        default: []
+    }
 
- const cartModel = model("carts", cartSchema)
 
- export default cartModel
+})
+
+cartSchema.pre('findOne', function () {
+    this.populate('products.id_prod')
+})
+
+const cartModel = model("carts", cartSchema)
+
+export default cartModel
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,81 +1,74 @@
-import { Router } from 'express';
-import productModel from '../models/products';
+import { Router } from "express";
+import productModel from "../models/product.js";
 
-const productsRouter = Router();
+const productsRouter = Router()
 
 productsRouter.get('/', async (req, res) => {
     try {
-        const { limit } = req.query;
-        const prods = await productModel.find()
-        let limite = parseInt(limit);
-        if (!limite) {
-            limite = prods.length;
-        }
-        const prodsLimit = prods.slice(0, limite);
-        res.status(200).render(prodsLimit);
-    } catch (error) {
-        res.status(500).render(`Error interno del Servidor ${error}`);
-    }
-});
+        const { limit } = req.query
+        const prods = await productModel.find().lean()
+        let limite = parseInt(limit)
+        if (!limite)
+            limite = prods.length
+        const prodsLimit = prods.slice(0, limite)
+        res.status(200).render('templates/home', {
+            mostrarProductos: true,
+            productos: prodsLimit,
+            css: 'product.css'
+        })
 
+    } catch (error) {
+        res.status(500).render('templates/error', {
+            error: error,
+        })
+    }
+})
+
+//: significa que es modificable (puede ser un 4 como un 10 como un 75)
 productsRouter.get('/:pid', async (req, res) => {
     try {
-        const idProducto = req.params.pid;
+        const idProducto = req.params.pid //Todo dato que se consulta desde un parametro es un string
         const prod = await productModel.findById(idProducto)
-
-        if (prod) {
-            res.status(200).render(prod);
-        } else {
-            res.status(400).render("Producto no existe");
-        }
+        if (prod)
+            res.status(200).send(prod)
+        else
+            res.status(404).send("Producto no existe")
     } catch (error) {
-        res.status(500).render(`Error interno del Servidor al intentar obtener producto: ${error}`);
+        res.status(500).send(`Error interno del servidor al consultar producto: ${error}`)
     }
-});
+})
 
 productsRouter.post('/', async (req, res) => {
     try {
-        const product = req.body;
+        const product = req.body
         const mensaje = await productModel.create(product)
-        if (mensaje === "Producto creado correctamente") {
-            res.status(200).render(mensaje);
-        } else {
-            res.status(400).render(mensaje);
-        }
+        res.status(201).send(mensaje)
+
     } catch (error) {
-        res.status(500).render(`Error interno del Servidor al intentar añadir producto: ${error}`);
+        res.status(500).send(`Error interno del servidor al crear producto: ${error}`)
     }
-});
+})
 
 productsRouter.put('/:pid', async (req, res) => {
     try {
-        const idProducto = req.params.pid;
-        const updateProduct = req.body;
-        const mensaje = await productModel.findByIdAndUpdate(idProducto, updateProduct)
+        const idProducto = req.params.pid
+        const updateProduct = req.body
+        const prod = await productModel.findByIdAndUpdate(idProducto, updateProduct)
+        res.status(200).send(prod)
 
-        if (mensaje === "Producto actualizado correctamente") {
-            res.status(200).render(mensaje);
-        } else {
-            res.status(404).render(mensaje);
-        }
     } catch (error) {
-        res.status(500).render(`Error interno del Servidor al intentar actualizar producto: ${error}`);
+        res.status(500).send(`Error interno del servidor al actualizar producto: ${error}`)
     }
-});
+})
 
 productsRouter.delete('/:pid', async (req, res) => {
     try {
-        const idProducto = req.params.pid;
+        const idProducto = req.params.pid
         const mensaje = await productModel.findByIdAndDelete(idProducto)
-
-        if (mensaje === "Producto eliminado correctamente") {
-            res.status(200).render(mensaje);
-        } else {
-            res.status(404).render(mensaje);
-        }
+        res.status(200).send(mensaje)
     } catch (error) {
-        res.status(500).render(`Error interno del Servidor al intentar eliminar producto: ${error}`);
+        res.status(500).send(`Error interno del servidor al eliminar producto: ${error}`)
     }
-});
+})
 
-export default productsRouter;
+export default productsRouter
